ed19ebfb32856d0e0b428d4259f1dbb4ae2eb74e
Alexandru Ghilev <aghilev@yahoo.fr>
2023-12-08 17:12:51 +0100

    modif name function

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 class AdminController extends AbstractController
 {
-    public function showPanel(): string
+    public function index(): string
     {
         return $this->twig->render('AdminPanel/index.html.twig', []);
     }
