0cdaf32235d2a840d5e5b72416e9e13808d16d30
FH-dev <bobykarr@gmail.com>
2023-12-19 15:46:02 +0100

    add category, product, search functions features

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,6 @@ namespace App\Controller;
 
 use App\Model\ProductManager;
 use App\Service\SessionManager;
-use Symfony\Component\DomCrawler\Crawler;
-use Symfony\Component\HttpClient\HttpClient;
 
 class HomeController extends AbstractController
 {
@@ -28,34 +26,6 @@ class HomeController extends AbstractController
         return $this->twig->render('Home/index.html.twig', [
             'session' => $this->session,
             'products' => $products,
-        ]);
-    }
-
-    public function scrappy(): string
-    {
-        $client = HttpClient::create();
-        $response = $client->request('GET', 'http://www.radio-active.net/la-bande-des-comics_rss.php');
-        $rssContent = $response->getContent();
-
-        $crawler = new Crawler($rssContent);
-
-        $crawler->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
-        $crawler->registerNamespace('itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd');
-        $crawler->registerNamespace('googleplay', 'http://www.google.com/schemas/play-podcasts/1.0');
-
-        $items = $crawler->filterXPath('//atom:entry')->each(function (Crawler $node) {
-            $title = $node->filterXPath('itunes:title')->text();
-            $link = $node->filterXPath('link')->attr('href');
-
-            return [
-                'title' => $title,
-                'link' => $link,
-
-            ];
-        });
-        var_dump($items);
-        return $this->twig->render('Home/index.html.twig', [
-            'items' => $items,
         ]);
     }
 }
